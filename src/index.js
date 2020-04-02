effd0eefa3987e8f8c5aa574e9db2d23afe7c88f
Anton Zhilkin <tonyzhilkin@gmail.com>
2020-04-03 01:00:15 +0300

    Change name generator to regexp based

--- src/index.js
+++ src/index.js
@@ -3,22 +3,23 @@ import axios from 'axios';
 import url from 'url';
 import path from 'path';
 
-const generateFileName = (pageUrl) => {
-  const { hostname } = url.parse(pageUrl);
-  const fileName = hostname.split('.').join('-').concat('.html');
+const generateName = (pageUrl) => {
+  const lastSymbol = /[/]$/gi;
+  const nonSymbolDigit = /[^a-z\d]/gi;
+  const { hostname, pathname } = url.parse(pageUrl);
+  const pageLocation = hostname.concat(pathname);
+  const fileName = pageLocation.replace(lastSymbol, '').replace(nonSymbolDigit, '-').concat('.html');
   return fileName;
 };
-
 
 const pageLoad = (pageUrl, outputDirectory = process.cwd()) => {
   const http = axios.create({
     baseURL: pageUrl,
     timeout: 5000,
   });
-  const fileName = generateFileName(pageUrl);
+  const fileName = generateName(pageUrl);
   const filePath = path.join(outputDirectory, fileName);
   return http.get('/')
     .then(({ data }) => fs.writeFile(filePath, data));
 };
-
 export default pageLoad;
